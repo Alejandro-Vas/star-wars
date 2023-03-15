0c47a33f0f1fc9885144e4f41ec6160c591d8f1a
Aleksandr Vasin <zended@yandex.ru>
2023-03-15 16:11:04 +0300

    feat: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,29 @@
 import Logo from 'assets/icons/logo.svg';
 import styles from './styles.module.scss';
 
+const menuItems = [
+  {
+    title: 'Home',
+    path: '/',
+  },
+  {
+    title: 'Characters',
+    path: 'Characters',
+  },
+];
+
 function Header() {
   return (
     <div className={styles.root}>
       <div className={styles.logo}>
         <Logo />
+      </div>
+      <div className={styles.menu}>
+        {menuItems.map(({ title, path }) => (
+          <a href={path} className={styles.menuItem}>
+            {title}
+          </a>
+        ))}
       </div>
     </div>
   );
